7bd03bc0ba0ece678ce27cce19a1982aa4d4dd36
sangwook16074 <qazws78941@gmail.com>
2025-03-14 10:35:15 +0900

    앱 로고 변경

--- lib/src/ui/common/widgets/logo.dart
+++ lib/src/ui/common/widgets/logo.dart
@@ -7,26 +7,23 @@ class Logo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 120,
-      height: 120,
-      decoration: BoxDecoration(
-          color: const Color(0xffffffff),
-          borderRadius: BorderRadius.circular(24),
-          boxShadow: [
-            BoxShadow(
-                color: const Color(0xff000000).withOpacity(0.25),
-                offset: const Offset(0, 1),
-                blurRadius: 4,
-                spreadRadius: 0)
-          ]),
-      alignment: Alignment.center,
-      child: const Text(
-        "A&I",
-        style: TextStyle(
-            color: Color(0xff425065),
-            fontSize: 50,
-            fontWeight: FontWeight.bold),
-      ),
-    );
+        padding: const EdgeInsets.all(20.0),
+        width: 120,
+        height: 120,
+        decoration: BoxDecoration(
+            color: const Color(0xffffffff),
+            borderRadius: BorderRadius.circular(24),
+            boxShadow: [
+              BoxShadow(
+                  color: const Color(0xff000000).withOpacity(0.25),
+                  offset: const Offset(0, 1),
+                  blurRadius: 4,
+                  spreadRadius: 0)
+            ]),
+        alignment: Alignment.center,
+        child: SizedBox(
+          width: 200,
+          child: Image.asset("assets/logo.png"),
+        ));
   }
 }
