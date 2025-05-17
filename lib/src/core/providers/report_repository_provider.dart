049dae1fc2a4c375cd90416b35fdc2ca6ad93e92
sangwook16074 <qazws78941@gmail.com>
2025-05-17 14:51:18 +0900

    git ignore 적용

--- lib/src/core/providers/report_repository_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:a_and_i_report_web_server/src/feature/reports/data/repositories/report_repository.dart';
-import 'package:dio/dio.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-final reportRepositoryProvider = Provider<ReportRepository>(
-    (ref) => ReportRepository(Dio(), baseUrl: dotenv.get("BASE_URL")));
--- lib/src/core/theme/app_theme.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-final theme = ThemeData(
-    primaryColor: const Color(0xff000000),
-    useMaterial3: false,
-    scaffoldBackgroundColor: const Color(0xfff6f6f6));
