3057ed4e6b271bcc116e2b9966fa4d2a3a8c23ac
sangwook16074 <qazws78941@gmail.com>
2025-09-15 11:38:46 +0900

    앱바 UI 변경 및 웹 캐싱 관련 firebase.json 설정에서 캐싱 무효화 적용

--- lib/src/core/theme/app_theme.dart
+++ lib/src/core/theme/app_theme.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 final theme = ThemeData(
     primaryColor: const Color(0xff000000),
     useMaterial3: false,
+    appBarTheme: AppBarTheme(backgroundColor: Color(0xffffffff)),
     scaffoldBackgroundColor: const Color(0xfff6f6f6));
