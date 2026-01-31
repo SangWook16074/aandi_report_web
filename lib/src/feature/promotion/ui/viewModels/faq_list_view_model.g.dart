38cd422a2b588d1a84bd2c5b4d24cabde3429f72
sangwook16074 <qazws78941@gmail.com>
2026-01-31 17:13:24 +0900

    feat: Implement FAQ page with filtering, routing, and apply button

---
+++ lib/src/feature/promotion/ui/viewModels/faq_list_view_model.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'faq_list_view_model.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$faqListViewModelHash() => r'b22bc13d833e65b7da3ebf3826576e54bf332a4f';
+
+/// See also [FaqListViewModel].
+@ProviderFor(FaqListViewModel)
+final faqListViewModelProvider =
+    AutoDisposeNotifierProvider<FaqListViewModel, FaqListViewState>.internal(
+  FaqListViewModel.new,
+  name: r'faqListViewModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$faqListViewModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$FaqListViewModel = AutoDisposeNotifier<FaqListViewState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
--- lib/src/feature/promotion/ui/widgets/promotion_schedule.dart
+++ lib/src/feature/promotion/ui/widgets/promotion_schedule.dart
@@ -1,3 +1,4 @@
+import 'package:a_and_i_report_web_server/src/feature/promotion/ui/views/apply_button_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:a_and_i_report_web_server/src/core/widgets/responsive_layout.dart';
@@ -6,14 +7,6 @@ import 'package:url_launcher/url_launcher.dart';
 class PromotionSchedule extends StatelessWidget {
   final VoidCallback? onPrevPage;
   const PromotionSchedule({super.key, this.onPrevPage});
-
-  Future<void> _launchUrl() async {
-    final Uri url = Uri.parse(
-        'https://docs.google.com/forms/d/e/1FAIpQLSdIXB0FxwJaQw6f-vpf5mYBxNMlJs2PII_0UQo31n3As2PgyA/viewform?usp=header');
-    if (!await launchUrl(url)) {
-      throw Exception('Could not launch $url');
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -186,27 +179,9 @@ class PromotionSchedule extends StatelessWidget {
                             color: Colors.white,
                           ),
                         ),
-                        ElevatedButton(
-                          onPressed: _launchUrl,
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.white,
-                            foregroundColor: Colors.black,
-                            padding: EdgeInsets.symmetric(
-                                horizontal: isMobile ? 30 : 40,
-                                vertical: isMobile ? 16 : 20), // 반응형 패딩
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            elevation: 0,
-                          ),
-                          child: Text(
-                            '지금 지원하기',
-                            style: TextStyle(
-                              fontSize: isMobile ? 16 : 20, // 반응형 폰트 크기
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ).animate().shimmer(delay: 1500.ms, duration: 1000.ms),
+                        ApplyButtonView()
+                            .animate()
+                            .shimmer(delay: 1500.ms, duration: 1000.ms),
                       ],
                     ),
                   ),
